3726d2306facf4fafa0c5d48aac11e9f6371fa58
NickBecks <viscabarcelona97@mail.ru>
2016-02-04 22:06:15 +0200

    mycommit

---
+++ src/ua/com/nick/TopClass.java
@@ -0,0 +1,13 @@
+package ua.com.nick;
+
+public class TopClass {
+
+	public static void main(String[] args) {
+		tellTruth("Kolya");
+}
+	
+	public static void tellTruth(String name){
+		System.out.println(name+" is the best coder!");
+	}
+
+}
